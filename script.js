3567d656905594ddb80878ee71b3252f6fcfdccf
FedeWaelk <fedewaelk@gmail.com>
2025-01-21 11:53:06 -0300

    change book constructor for class

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
+class Book {
+    constructor(title, author, pages, status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+
+    toggleStatus() {
+        this.status = this.status === "read" ? "not read yet" : "read";
+    }
+}
+
 const myLibrary = [
     new Book("The Hobbit", "J.R.R. Tolkien", 310, "read"),
     new Book("1984", "George Orwell", 328, "not read yet"),
-    new Book("Dune", "Frank Herbert", 412, "not read yet"),
     new Book("The Last Wish", "Andrzej Sapkowski", 288, "read"),
+    new Book("Dune", "Frank Herbert", 412, "not read yet"),
 ];
-
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-}
 
 function addBookToLibrary(title, author, pages, status) {
     const newBook = new Book(title, author, pages, status);
     myLibrary.push(newBook);
 }
-
-Book.prototype.toggleStatus = function () {
-    this.status = this.status === "read" ? "not read yet" : "read";
-};
 
 function displayBooks() {
     const tableBody = document.querySelector('#library-table tbody');
